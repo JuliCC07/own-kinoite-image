89fe36e9ee16879ac01c38f2dc64887765584d68
JuliCC07 <lca6822@gmail.com>
2025-11-10 00:05:56 +0100

    Custom packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,6 +19,26 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+# Hardware-related Drivers
+dnf5 install -y \
+	acpid \
+	akmod-nvidia \
+	libvirt-daemon-config-network \
+	libvirt-daemon-kvm \
+	lm-sensors \
+	python-envycontrol \
+	qemu-kvm \
+	virt-manager \
+	xorg-x11-drv-nvidia-cuda \
+	xorg-x11-drv-nvidia-cuda-libs
+	powertop \
+	tuned-utils\
+
+# LenovoLegionLinux
+#dnf5 -y copr enable mrduarte/LenovoLegionLinux
+#dnf5 -y install dkms-LenovoLegionLinux
+#dnf5 -y copr disable mrduarte/LenovoLegionLinux
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
