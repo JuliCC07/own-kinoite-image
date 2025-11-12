a3d475f6f920a6e27ec992829aac0d7e9e620a0d
JuliCC <97172101+JuliCC07@users.noreply.github.com>
2025-11-12 17:41:07 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -34,8 +34,7 @@ dnf5 -y copr disable sunwire/envycontrol
 # dnf5 -y copr disable mrduarte/LenovoLegionLinux
 git clone https://github.com/johnfanv2/LenovoLegionLinux.git
 cd LenovoLegionLinux/kernel_module
-make
-install -Dm644 legion-laptop.ko /usr/lib/modules/<kernel>/kernel/drivers/platform/x86/
+make install
 
 #### Example for enabling a System Unit File
 
